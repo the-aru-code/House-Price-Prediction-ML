10eb96c31311d5f0dec9629832776c201cdf90b1
the-aru-code <saggopandey@gmail.com>
2025-12-28 16:08:33 +0530

    Fix house price prediction output indexing

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ final_value = scaler.transform([all_value])
 
 model = RandomForestRegressor()
 model.fit(X,y)
-house_price = model.predict(final_value)
+house_price = model.predict(final_value)[0][0]
 
 with st.spinner('Predicting House Price'):
   time.sleep(1)
@@ -37,6 +37,7 @@ msg = f'''House price is: ${house_price*100000}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Arushi Pandey**''')
+
 
 
 
