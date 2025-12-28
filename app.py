2cf1aa325f75a4ea1d504bbdfe09266815e62fe6
the-aru-code <saggopandey@gmail.com>
2025-12-28 15:54:29 +0530

    Implement house price prediction using RandomForest
    Added RandomForestRegressor model for house price prediction and included data scaling.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
 
 st.title('🏠House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
@@ -22,5 +23,17 @@ for i in X:
   all_value.append(ans)
 
 # st.write(all_value)
+scaler = StandardScaler()
+scaled_X = scaler.fit_transform(X)
+final_value = scaler.transform([all_value])
+
+model = RandomForestRegressor()
+model.fit(X,y)
+house_price = model.predict(final_value)
+
+with st.spinner('Predicting House Price'):
+  time.sleep()
+st.write(house_price)
+
 
 
