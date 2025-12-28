24cfdbb878380837b8e91d6545fa6860afda8b82
the-aru-code <saggopandey@gmail.com>
2025-12-28 14:59:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ import time
 from sklearn.datasets import fetch_california_housing
 
 st.title('🏠House Price prediction using ML')
+st.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
