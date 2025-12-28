458fe9fca71e33b79a7b6d0828484a8ca14a96cc
the-aru-code <saggopandey@gmail.com>
2025-12-28 15:14:08 +0530

    Implement house feature selection in Streamlit app
    Added functionality to read house data and select features using sliders.

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ from sklearn.datasets import fetch_california_housing
 
 st.title('🏠House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
+
+df = pd.read_csv('house_data.csv')
+X = df.iloc[:,:-3]
+y = df.iloc[:,:-1]
+
+st.sidebar.title('🏡 Select House features 🏡')
+st.sidebar.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
+all_value = []
+for i in X:
+  ans = st.sidebar.slider(f'Select {i} value')
+  all_value.append(ans)
+
+st.write(all_value)
+
